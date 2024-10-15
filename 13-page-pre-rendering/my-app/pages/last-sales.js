0a94a2b1bbd710da014f67e772d7bb1c95b24af3
Yanbin Zuo <jimmyzuo123456@gmail.com>
2024-10-15 15:21:58 -0700

    implementing client-side data fetching

--- 13-page-pre-rendering/my-app/pages/last-sales.js
+++ 13-page-pre-rendering/my-app/pages/last-sales.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function LastSalesPage() {
+  const [sales, setSales] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  console.log(">>> sales: ", sales);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://nextjs-project-c1b4b-default-rtdb.firebaseio.com/sales.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const fetchedSales = [];
+        // data from Firebase is object, we are converting it to array
+        for (const key in data) {
+          fetchedSales.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume,
+          });
+        }
+        setSales(fetchedSales);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if(!sales) {
+    return<p>No data yet</p>
+  }
+
   return (
-    <div>LastSalesPage</div>
-  )
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          <p>userName: {sale.username}</p>
+          <p>Volume: {sale.volume}</p>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default LastSalesPage
-
+export default LastSalesPage;
