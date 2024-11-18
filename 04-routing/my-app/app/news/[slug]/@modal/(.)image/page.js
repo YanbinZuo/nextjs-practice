568a0fbc4a5d544107f6156fff11a05a41987a4e
Yanbin Zuo <jimmyzuo123456@gmail.com>
2024-11-18 11:27:08 -0800

    modal update

--- 04-routing/my-app/app/news/[slug]/@modal/(.)image/page.js
+++ 04-routing/my-app/app/news/[slug]/@modal/(.)image/page.js
@@ -1,7 +1,10 @@
+"use client";
 import { DUMMY_NEWS } from "@/dummy-news";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 function InterceptedFullScreenImagePage({ params }) {
+  const router = useRouter
   const newsSlug = params.slug;
   const newsItem = DUMMY_NEWS.find((news) => news.slug === newsSlug);
 
@@ -14,7 +17,7 @@ function InterceptedFullScreenImagePage({ params }) {
 
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back}/>
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
