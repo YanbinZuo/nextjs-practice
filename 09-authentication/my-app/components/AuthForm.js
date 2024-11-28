42e605f62259e366a5682dfca89cf82fda9016c8
Yanbin Zuo <jimmyzuo123456@gmail.com>
2024-11-27 18:00:25 -0800

    authentication start project

---
+++ 09-authentication/my-app/components/AuthForm.js
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+const AuthForm = () => {
+  return (
+    <form id="auth-form">
+      <div>
+        <img src="/images/auth-icon.jpg" alt="A lock icon" />
+      </div>
+      <p>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
+      </p>
+      <p>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+      </p>
+      <p>
+        <button type="submit">Create Account</button>
+      </p>
+      <p>
+        <Link href="/">Login with existing account</Link>
+      </p>
+    </form>
+  );
+};
+
+export default AuthForm;
